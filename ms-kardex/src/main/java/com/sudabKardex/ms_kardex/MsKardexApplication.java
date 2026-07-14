22cf2c930f90da289de3fc49a0f0da7df5375226
Renato Meza <254360150+renatomeza17@users.noreply.github.com>
2026-07-14 07:47:13 -0500

    Add anotacion en kardex principal

--- ms-kardex/src/main/java/com/sudabKardex/ms_kardex/MsKardexApplication.java
+++ ms-kardex/src/main/java/com/sudabKardex/ms_kardex/MsKardexApplication.java
@@ -2,8 +2,10 @@ package com.sudabKardex.ms_kardex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MsKardexApplication {
 
 	public static void main(String[] args) {
