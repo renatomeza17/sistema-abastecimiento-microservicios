c5a573268677af3a16bc4c57dc4d18fd41051f46
Alisson Cabanillas <alisson.cabanillas@unmsm.edu.pe>
2026-07-14 03:31:59 -0500

    refactor(login): eliminar entidad Persona y su repositorio

--- ms-login/src/main/java/com/sudabLogin/ms_login/repository/PersonaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sudabLogin.ms_login.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sudabLogin.ms_login.model.Persona;
-
-public interface PersonaRepository extends JpaRepository<Persona, Long> {
-}
