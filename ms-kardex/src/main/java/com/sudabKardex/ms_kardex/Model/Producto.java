14f519a125de2e0fe8bb66387017b8a74ec597d0
Renato Meza <254360150+renatomeza17@users.noreply.github.com>
2026-07-14 07:31:24 -0500

    Confg en producto

--- ms-kardex/src/main/java/com/sudabKardex/ms_kardex/Model/Producto.java
+++ ms-kardex/src/main/java/com/sudabKardex/ms_kardex/Model/Producto.java
@@ -1,11 +1,12 @@
 package com.sudabKardex.ms_kardex.Model;
 
-import org.springframework.data.annotation.Id;
+
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
