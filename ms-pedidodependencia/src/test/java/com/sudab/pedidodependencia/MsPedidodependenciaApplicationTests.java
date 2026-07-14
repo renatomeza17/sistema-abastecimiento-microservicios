93e010de656c21323c269bb3213e90b66f38847a
Mitchell <sihuinchapalacinmitchellbruce@gmail.com>
2026-07-14 06:54:40 -0500

    Se agregan los microservicios de requerimiento y pedido dependencia, se eliminan los microservicios de login y se agregan los archivos de despliegue en kubernetes para los microservicios de requerimiento y pedido dependencia.

--- ms-pedidodependencia/src/test/java/com/sudab/pedidodependencia/MsPedidodependenciaApplicationTests.java
+++ ms-pedidodependencia/src/test/java/com/sudab/pedidodependencia/MsPedidodependenciaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.sudabRequerimiento.ms_requerimiento;
+package com.sudab.pedidodependencia;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MsRequerimientoApplicationTests {
+class MsPedidodependenciaApplicationTests {
 
 	@Test
 	void contextLoads() {
--- ms-requerimiento/src/test/java/com/sudab/requerimientos/MsRequerimientoApplicationTests.java
+++ ms-requerimiento/src/test/java/com/sudab/requerimientos/MsRequerimientoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.sudabPedidoDependencia.ms_pedidodependencia;
+package com.sudab.requerimientos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MsPedidodependenciaApplicationTests {
+class MsRequerimientoApplicationTests {
 
 	@Test
 	void contextLoads() {
