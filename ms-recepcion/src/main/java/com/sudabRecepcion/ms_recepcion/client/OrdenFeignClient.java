7cfdcd4c97a1629a8a58595745af8975d7c5ee0c
Alisson Cabanillas <alisson.cabanillas@unmsm.edu.pe>
2026-07-17 20:57:55 -0500

    fix(ms-recepcion): eliminar url hardcodeada en @FeignClient para habilitar descubrimiento Eureka

--- ms-recepcion/src/main/java/com/sudabRecepcion/ms_recepcion/client/OrdenFeignClient.java
+++ ms-recepcion/src/main/java/com/sudabRecepcion/ms_recepcion/client/OrdenFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
  
 import com.sudabRecepcion.ms_recepcion.dto.OrdenResponseDTO;
  
-@FeignClient(name = "MS-ORDENES", url = "${ms-ordenes.url:http://localhost:8082}")
+@FeignClient(name = "MS-ORDENES")
 public interface OrdenFeignClient {
  
     @GetMapping("/api/ordenes/{id}")
